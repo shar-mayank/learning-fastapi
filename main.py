57ffc3b3b9a163ed70d07a0980fb489de106bbe7
Mayank Sharma <sharmayank1608@gmail.com>
2024-05-25 14:29:10 +0530

    CRUD testing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import pos
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,19 +12,25 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "Hello", "content": "example", "id": 1}, {"title": "Hello-1", "content": "example-1", "id": 2}]
+
 @app.get("/")
 async def root():
     return {"message": "Hello ji ki haal chaal"}
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "this is a post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 async def create_post(posts: Post):
-    print(f"Post title: {posts.title}")
-    print(f"Post content: {posts.description}")
-    print(f"Post published: {posts.published}")
-    print(f"Post rating: {posts.rating}")
-    print(posts.model_dump())
-    return {"data": "post sent to server"}
+    # print(f"Post title: {posts.title}")
+    # print(f"Post content: {posts.description}")
+    # print(f"Post published: {posts.published}")
+    # print(f"Post rating: {posts.rating}")
+    # print(posts.model_dump())
+
+    post_dict = posts.model_dump()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
